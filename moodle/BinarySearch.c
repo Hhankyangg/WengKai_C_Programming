0287a4f683b23c83c4257f98f03f299475953a48
Hank <hankyang428@gmail.com>
2023-04-25 19:27:46 +0800

    fix the bug in moddle/BinarySearch.c

--- moodle/BinarySearch.c
+++ moodle/BinarySearch.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
 
-int main() {
-	int array[21] = {10,11,12,13,14,15,16,17,18,19,110,111,112,113,114,115,116,117,118,119,120};
-	int key, ret = -1;
-	scanf("%d", &key);
-	int leftnum = 0;
-	int rightnum = sizeof(array)/sizeof(array[0])-1;
-	while (rightnum > leftnum) {
-		if (array[(rightnum+leftnum)/2] < key) {
-			leftnum = (rightnum+leftnum)/2+1;
-		} 
-		else if (array[(rightnum+leftnum)/2] > key) {
-			rightnum = (rightnum+leftnum)/2-1;
-		} 
+int BinarySearch(int list[], int leftnum, int rightnum, int x) {
+	while (leftnum <= rightnum) {
+		int mid = (leftnum + rightnum) / 2;
+		if (x == list[mid]) {
+			return mid;
+		}
+		else if (x < list[mid]) {
+			rightnum = mid - 1;
+		}
 		else {
-			ret = (rightnum+leftnum)/2;
-			break;
+			leftnum = mid + 1;
 		}
 	}
-	printf("%d", ret);
-	return 0;
+	return -1;	// if not find
 }
+
 
